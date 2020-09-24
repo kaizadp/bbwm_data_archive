7f4f6e6f9a7c39c1a66953c7f5f65eebbd83e399
kaizadp <50244730+kaizadp@users.noreply.github.com>
2020-09-24 07:02:13 -0400

    create qaqc graphs for dep/stream

--- code/2-graphs.R
+++ code/2-graphs.R
@@ -1,6 +1,17 @@
 library(drake)
 library(tidyverse)
 
+STREAMS_ANNUAL = "data/streams/bbwm_stream_annual.csv"
+STREAMS_MONTHLY = "data/streams/bbwm_stream_manual.csv"
+STREAMS_ALL = "data/streams/bbwm_stream_all.csv"
+DEP_ANNUAL = "data/deposition/bbwm_deposition_annual.csv"
+DEP_MONTHLY = "data/deposition/bbwm_deposition_monthly.csv"
+DEP_ALL = "data/deposition/bbwm_deposition_all.csv"
+
+
+
+
+
 # FUNCTIONS ---------------------------------------------------------------
 theme_kp <- function() {  # this for all the elements common across plots
   theme_bw() %+replace%
@@ -26,7 +37,7 @@ theme_kp <- function() {  # this for all the elements common across plots
 }
 
 clean_dep_annual = function(dep_annual_temp){
-    dep_annual_temp %>% 
+  dep_annual_temp %>% 
     select(WY, NO3_eq_ha, SO4_eq_ha) %>% 
     rename(nitrate = NO3_eq_ha,
            sulfate = SO4_eq_ha)
@@ -60,10 +71,10 @@ clean_dep_annual2 = function(dep_annual_temp){
            S_in = WB_S) %>% 
     mutate(watershed="WB")
   
-rbind(dep_EB_temp, dep_WB_temp)
+  rbind(dep_EB_temp, dep_WB_temp)
 }
 clean_stream_annual2 = function(stream_annual_temp){
-    stream_annual_temp %>% 
+  stream_annual_temp %>% 
     select(WY, watershed, NH4_eq_ha, NO3_eq_ha, SO4_eq_ha) %>% 
     mutate(NH4_eq_ha = replace_na(NH4_eq_ha, 0),
            N_out = (NH4_eq_ha+NO3_eq_ha),
@@ -132,5 +143,78 @@ ms_figs_plan = drake_plan(
     knitr_in("reports/manuscript_figures.Rmd"),
     output_format = rmarkdown::github_document())
 )
+
+
 # make plan ----
 make(ms_figs_plan)
+
+
+# qaqc plan ---------------------------------------------------------------
+
+qaqc_plan = drake_plan(
+  # dep ---------------------------------------------------------------------
+  dep_all = read.csv(file_in(DEP_ALL)),
+  
+  gg_dep_all = dep_all %>% 
+    dplyr::select(-record_type, -month, -day, -julian_day, -running_day_of_project) %>% 
+    pivot_longer(-c("site", "year"))  %>% 
+    ggplot(aes(x = year, y = value))+
+    geom_point()+
+    facet_wrap(~name, scales = "free_y"),
+  
+  dep_annual = read.csv(file_in(DEP_ANNUAL)),  
+  gg_dep_annual = 
+    dep_annual %>% 
+    pivot_longer(-WY) %>% 
+    ggplot(aes(x = WY, y = value))+
+    geom_point()+ geom_path()+
+    facet_wrap(~name, scales = "free_y"),
+  
+  dep_monthly = read.csv(file_in(DEP_MONTHLY)),
+  gg_dep_monthly = 
+     dep_monthly %>% 
+     pivot_longer(-c(year,month)) %>% 
+     ggplot(aes(x = year, y = value))+
+     geom_point()+
+     facet_wrap(~name, scales = "free_y"),
+  
+  #
+  # streams -----------------------------------------------------------------
+  
+  streams_annual = read.csv(file_in(STREAMS_ANNUAL)),
+  gg_streams_annual = 
+    streams_annual %>% 
+    pivot_longer(-c(WY, watershed)) %>% 
+    ggplot(aes(x = WY, y = value, color = watershed))+
+    geom_point()+geom_path()+
+    facet_wrap(~name, scales = "free_y")+
+    NULL,
+  
+  streams_monthly = read.csv(file_in(STREAMS_MONTHLY)),
+  gg_streams_monthly = 
+    streams_monthly %>% 
+    pivot_longer(-c(year, month, watershed)) %>% 
+    ggplot(aes(x = year, y = value, color = watershed))+
+    geom_point()+
+    facet_wrap(~name, scales = "free_y")+
+    NULL,
+  
+  streams_all = read.csv(file_in(STREAMS_ALL)),
+  gg_streams_all = 
+    streams_all %>% 
+    dplyr::select(year, watershed, ends_with("ueq_L")) %>% 
+    reshape2::melt(id = c("watershed", "year"),
+                   variable.name = "name", value.name = "value") %>% 
+    ggplot(aes(x = year, y = value, color = watershed))+
+    geom_point()+
+    facet_wrap(~name, scales = "free_y")+
+    NULL,
+  
+  
+  # report ------------------------------------------------------------------
+  report = rmarkdown::render(
+    knitr_in("reports/qaqc_figures.Rmd"),
+    output_format = rmarkdown::github_document())
+)
+
+make(qaqc_plan)
---
+++ reports/qaqc_figures.md
@@ -0,0 +1,20 @@
+QA-QC figures
+================
+
+## DEPOSITION
+
+![](markdown-figs/qaqc/dep_annual-1.png)<!-- -->
+
+![](markdown-figs/qaqc/dep_monthly-1.png)<!-- -->
+
+![](markdown-figs/qaqc/dep_all-1.png)<!-- -->
+
+## STREAMS
+
+![](markdown-figs/qaqc/streams_annual-1.png)<!-- -->
+
+![](markdown-figs/qaqc/streams_monthly-1.png)<!-- -->
+
+![](markdown-figs/qaqc/streams_all-1.png)<!-- -->
+
+## SOILS
